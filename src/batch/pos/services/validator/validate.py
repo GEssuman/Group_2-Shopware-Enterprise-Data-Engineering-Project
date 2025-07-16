f10be0ed5be9a31efba089bd238eb78a664c38ea
GEssuman <godsaves.essuman@amalitechtraining.org>
2025-07-16 11:00:58 +0000

    feat(POS-validator):add logic to quarantine invalid files to s3 bucket

--- src/batch/pos/services/validator/validate.py
+++ src/batch/pos/services/validator/validate.py
@@ -5,6 +5,7 @@ import logging
 from dotenv import load_dotenv
 from botocore.exceptions import BotoCoreError, ClientError
 from io import StringIO
+from datetime import datetime
 # Load env variables from .env file
 load_dotenv()
 bucket_name = os.getenv("AWS_S3_BUCKET")
@@ -60,18 +61,44 @@ def validate_file(df, filename):
     # check required columns
     missing_cols = set(schema["required_columns"]) - set(df.columns)
     if missing_cols:
-        logger.error(f"Validation failed for {filename}: missing required columns: {missing_cols}")
-        return False
+        reason = f"Missing required columns: {missing_cols}"
+        logger.error(f"Validation failed for {filename}: {reason}")
+        return False, reason
 
     # check for not-null columns
     for col in schema["not_nulls"]:
         if df[col].isnull().any():
-            logger.error(f"Validation failed for {filename}: column '{col}' contains null values")
-            return False
+            reason = f"Column '{col}' contains null values"
+            logger.error(f"Validation failed for {filename}: {reason}")
+            return False, reason
         
     logger.info(f"File '{filename}' passed validation")
-    return True
-    
+    return True, None
+
+def quarantine_file(bucket, original_key ,reason):
+    """
+    Move the invalid file to a 'quarantine/' folder in the same S3 bucket.
+    """
+    try:
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = os.path.basename(original_key)
+        quarantine_key = f"quarantine/{timestamp}_{filename}"
+
+        logger.warning(f"Quarantining file: {original_key} to {quarantine_key} | Reason: {reason}")
+        
+        # Copy file to quarantine location
+        s3.copy_object(
+            Bucket=bucket,
+            CopySource={'Bucket': bucket, 'Key': original_key},
+            Key=quarantine_key
+        )
+
+        # Optionally delete original (to simulate 'move')
+        s3.delete_object(Bucket=bucket, Key=original_key)
+        logger.info(f"File {original_key} moved to quarantine")
+
+    except Exception as e:
+        logger.error(f"Failed to quarantine file {original_key}: {e}")
 
 def main():
     if not bucket_name:
@@ -85,8 +112,15 @@ def main():
         return
     
     for file in files:
+
         df = download_from_s3(f"s3://{bucket_name}/{file}")
-        validate_file(df, file)
+        if df is None:
+            quarantine_file(bucket_name, file, "Failed to load file as DataFrame")
+            continue
+
+        is_valid, reason = validate_file(df, file)
+        if not is_valid:
+            quarantine_file(bucket_name, file, reason)
     
 
     
