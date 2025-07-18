faf3ed90d23eac74fa27d6869f9c988378c507a0
GEssuman <godsaves.essuman@amalitechtraining.org>
2025-07-18 04:26:13 +0000

    feat(POS-transformation): implement archive all processed files and include archive bucket as an argument in runtime

--- src/batch/pos/services/glue_job/transfom_pos.py
+++ src/batch/pos/services/glue_job/transfom_pos.py
@@ -19,10 +19,11 @@ spark = glueContext.spark_session
 
 
 # Get job arguments
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'POS_S3_BUCKET', 'POS_DELTA_BUCKET'])
-delta_path = f"s3://{args['POS_DELTA_BUCKET']}/pos"
-
-s3_input_path = f"s3://{args['POS_S3_BUCKET']}/pos"
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'POS_S3_BUCKET', 'POS_DELTA_BUCKET','ARCHIVE_BUCKET'])
+delta_path = f"s3://{args['ETL_LANDING_S3_BUCKET']}landing_zone/processed/pos"
+archive_bucket = args['ARCHIVE_BUCET']
+source_bucket = args['POS_S3_BUCKET']
+s3_input_path = f"s3://{source_bucket}/pos"
 
 schema = StructType([
     StructField("transaction_id", StringType()),
@@ -50,16 +51,57 @@ def read_csv_files(path):
         logging.error(f"Failed to read CSVs from {path}: {e}")
         raise
 
+def archive_all_csv_files(source_bucket, prefix, archive_bucket):
+    """
+    Archive all CSV files under a given prefix in an S3 bucket.
+
+    :param bucket_name: str, e.g., "ecommerce-raw.amalitech-gke"
+    :param prefix: str, e.g., "orders/" or "products/"
+    :param archive_prefix: str, archive base folder (defaults to 'archive/')
+    """
+    logging.info(f"Archiving files in bucket: {source_bucket}, prefix: {prefix}")
+    s3 = boto3.client("s3")
+
+    try:
+        response = s3.list_objects_v2(Bucket=source_bucket, Prefix=prefix)
+        if "Contents" not in response:
+            logging.info(f"No files found under {prefix}")
+            return
+
+        for obj in response["Contents"]:
+            key = obj["Key"]
+            if key.endswith(".csv"):
+                dest_key = prefix + key
+                logging.info(f"Archiving {key} → {dest_key}")
+                
+                # Copy to archive
+                s3.copy_object(
+                    Bucket=archive_bucket,
+                    CopySource={"Bucket": source_bucket, "Key": key},
+                    Key=dest_key
+                )
+
+                # Delete original
+                s3.delete_object(Bucket=source_bucket, Key=key)
+
+        logging.info(f"All CSV files under '{prefix}' archived successfully.")
+
+    except Exception as e:
+        logging.error(f"Failed to archive files under {prefix}: {e}")
+
+
 
 def main():
     df = read_csv_files(s3_input_path)
     df = df.withColumn("timestamp", F.from_unixtime(F.col("timestamp")).cast("timestamp"))\
             .withColumn("date", F.to_date("timestamp"))
-
+    df = df.dropDuplicates()
     df.write.format("delta") \
             .partitionBy("date") \
             .mode("append") \
             .save(delta_path)
+    
+    archive_all_csv_files(source_bucket, "pos", archive_bucket)
 
 if __name__ == "__main__":
     main()
